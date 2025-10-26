fbdd2c274bc2ee3275a93d3e71b1ff153a6da43d
tynvalis <tynvalis@gmail.com>
2025-10-26 14:02:08 +0100

    faet:add streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+    console.log('Enter text to reverse it. Press Ctrl+C to exit.');
+
+  
+    const reverseTransform = new Transform({
+        transform(chunk, _encoding, callback) {
+         
+            const reversedText = chunk.toString().trim().split('').reverse().join('');
+            this.push(reversedText + '\n');
+            callback();
+        }
+    });
+
+ 
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
