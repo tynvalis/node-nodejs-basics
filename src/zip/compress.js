6b222fabdf7b84e93b3939329a5a732ee1c695a6
tynvalis <tynvalis@gmail.com>
2025-10-26 18:54:10 +0100

    feat:add worker and zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
 const compress = async () => {
-  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const sourceFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const destinationFilePath = path.join(__dirname, 'files', 'archive.gz');
+
+  try {
+    console.log('Starting compression...');
+
+    const sourceStream = createReadStream(sourceFilePath);
+    const gzipStream = createGzip();
+    const destinationStream = createWriteStream(destinationFilePath);
+
+    await pipeline(sourceStream, gzipStream, destinationStream);
+
+    console.log('File compressed successfully!');
+    console.log(`Output: ${destinationFilePath}`);
+  } catch (error) {
+    console.error('An error occurred during compression:', error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
 const decompress = async () => {
-  // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const sourceFilePath = path.join(__dirname, 'files', 'archive.gz');
+
+  const destinationFilePath = path.join(
+    __dirname,
+    'files',
+    'fileToCompress.txt'
+  );
+
+  try {
+    console.log('Starting decompression...');
+
+    const sourceStream = createReadStream(sourceFilePath);
+    const gunzipStream = createGunzip();
+    const destinationStream = createWriteStream(destinationFilePath);
+
+    await pipeline(sourceStream, gunzipStream, destinationStream);
+
+    console.log('File decompressed successfully!');
+    console.log(`Output: ${destinationFilePath}`);
+  } catch (error) {
+    console.error('An error occurred during decompression:', error);
+  }
 };
 
 await decompress();
